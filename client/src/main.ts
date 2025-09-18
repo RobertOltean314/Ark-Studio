9c9e216e5bf4e2d65a6306849922f3d8f8d14e3c
RobertOLtean314 <olteanrobertadrian314@gmail.com>
2025-09-18 13:30:59 +0300

    reorganized files in the project

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,15 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { AppComponent } from './app/appComponent/app.component';
+import { AppComponent } from './app/app.component';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { environment } from './environments/environment';
+import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter([]),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth())
     // Add provideFirestore(() => getFirestore()) if using Firestore
   ]
 }).catch(err => console.error(err));
+
